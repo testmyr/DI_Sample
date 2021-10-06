975b9e7b7011448dd781401ade6a783ad17d60e2
Andrij Myronyuk <andrijmyronyuk@gmail.com>
2021-10-06 14:17:33 +0300

    Minor improvement at Presenter.

--- DI_Sample/DI_Sample/MVP/ReposListPresenter.swift
+++ DI_Sample/DI_Sample/MVP/ReposListPresenter.swift
@@ -35,10 +35,15 @@ class ReposListPresenter {
     func start(completion: (()->Void)? = nil) {
         let _ = dataPump.fetch(pageWithIndex: 1) { [weak self] response in
             if let weakSelf = self {
-                if let newlyFetchedModels = response?.reposArray, newlyFetchedModels.count > 0 {
-                    weakSelf.models = newlyFetchedModels.map({RepoModel(name: $0.name, description: $0.description)})
-                    weakSelf.reachedTheEndOfList = newlyFetchedModels.count < weakSelf.numberOfReposPerPage
-                    weakSelf.view.updateView()
+                if let newlyFetchedModels = response?.reposArray {
+                    if newlyFetchedModels.count > 0 {
+                        weakSelf.models = newlyFetchedModels.map({RepoModel(name: $0.name, description: $0.description)})
+                        weakSelf.reachedTheEndOfList = newlyFetchedModels.count < weakSelf.numberOfReposPerPage
+                        weakSelf.view.updateView()
+                    } else {
+                        weakSelf.view.showErrorAlert(errorText: "No repositories.")
+                        weakSelf.reachedTheEndOfList = true
+                    }
                 } else if let errorInfo = response?.errorMessage {
                     weakSelf.view.showErrorAlert(errorText: errorInfo.message)
                 }
@@ -61,11 +66,17 @@ class ReposListPresenter {
         queue = dataPump.fetch(pageWithIndex: duePageIndex) { [weak self] response in
             if let weakSelf = self {
                 let syncUpateOperation = BlockOperation(block: {
-                    if let models = response?.reposArray, models.count > 0 {
-                        let newlyFetchedModels = models.map({RepoModel(name: $0.name, description: $0.description)})
-                        weakSelf.models.append(contentsOf: newlyFetchedModels)
-                        weakSelf.reachedTheEndOfList = newlyFetchedModels.count < weakSelf.numberOfReposPerPage
-                        self?.view?.updateViewSync()
+                    if let models = response?.reposArray {
+                        if models.count > 0 {
+                            let newlyFetchedModels = models.map({RepoModel(name: $0.name, description: $0.description)})
+                            weakSelf.models.append(contentsOf: newlyFetchedModels)
+                            weakSelf.reachedTheEndOfList = newlyFetchedModels.count < weakSelf.numberOfReposPerPage
+                            self?.view?.updateViewSync()
+                        } else {
+                            weakSelf.reachedTheEndOfList = true
+                        }
+                    } else if let errorInfo = response?.errorMessage {
+                        weakSelf.view.showErrorAlert(errorText: errorInfo.message)
                     }
                 })
                 
